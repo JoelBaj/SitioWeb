28207dc09fc9992879f20c05e39ac7e2eabd60df
JoelBaj <jbbg.bajana@gmail.com>
2023-10-17 16:18:01 -0500

    subiendo todo

---
+++ js/questions.js
@@ -0,0 +1,21 @@
+(function () {
+    const tittleQuestions = [...document.querySelectorAll('.questions__title')];
+
+    tittleQuestions.forEach(question => {
+        question.addEventListener('click', () => {
+
+            let height = 0;
+            let answer = question.nextElementSibling;
+            let addPadding = question.parentElement.parentElement;
+
+            addPadding.classList.toggle('questions__padding--add');
+            question.children[0].classList.toggle('question__arrow--rotate');
+
+            if (answer.clientHeight === 0) {
+                height = answer.scrollHeight;
+            }
+            answer.style.height = `${height}px`;
+        });
+    });
+
+})();
